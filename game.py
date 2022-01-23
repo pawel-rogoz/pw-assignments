c28ba5b769323854a1a358c6defbe0e882bd690f
progoz <pawel.rogoz.stud@pw.edu.pl>
2022-01-23 22:17:53 +0100

    Changed the name and wau how draw_pokemons works Added new Exception

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from pokemon import Pokemon
 from random import sample
 import os
 
-class NoPokemonsError(Exception):
+class NotEnoughPokemonsError(Exception):
     pass
 
 class Game:
@@ -110,38 +110,12 @@ class Game:
         elif player_choice == "2": against_player.selected_pokemon().decrease_hp(1)
         elif player_choice == "3": pass
         elif player_choice == "4": self.choose_pokemon(self._first_player)
-        # match player_choice:
-        #     case "1":
-        #         player.selected_pokemon().increase_defence(10)
-        #     case "2":
-        #         #against player selected pokemon set hp cause of damage
-        #         against_player.selected_pokemon().decrease_hp(1)
-        #     case "3":
-        #         pass
-        #     case "4":
-        #         self.choose_pokemon(self._first_player)
 
-    def draw_pokemons(self, file):
+
+    def select_random_pokemons(self, file, length):
         list_of_pokemons = load_from_csv(file)
-        game_pokemons = sample(list_of_pokemons, 20)
+        try:
+            game_pokemons = sample(list_of_pokemons, length)
+        except NotEnoughPokemonsError:
+            print('Not enough pokemons in file to play game')
         return game_pokemons
-
-    def play(self):
-        pokemons = self.pokemons()
-        # pokemons_names = {pokemon.name():pokemon for pokemon in pokemons}
-
-        print(f'Battle: {self.first_player.name()} vs {self.second_player.name()}')
-
-        self.choose_pokemons(self._first_player)
-        self.choose_pokemons(self._second_player)
-        self.choose_pokemon(self._first_player)
-        self.choose_pokemon(self._second_player)
-
-        current = self.first_player()
-        against = self.second_player()
-
-        while current.has_alive_pokemons(): #czy current ma pokemony z hp>0
-            self.round(current, against)
-            temp = current
-            current = against
-            against = temp
