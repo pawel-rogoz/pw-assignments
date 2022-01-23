ce46d5b399f0a20809344e187d5d900c67fdd71d
progoz <pawel.rogoz.stud@pw.edu.pl>
2022-01-23 22:45:32 +0100

    Added clear_pokemons method

--- player.py
+++ player.py
@@ -11,20 +11,6 @@ class Player:
     def pokemons(self):
         return self._pokemons
 
-    def remove_pokemons(self):
+    def clear_pokemons(self):
         self._pokemons = []
 
-    def has_alive_pokemons(self):
-        for pokemon in self.pokemons():
-            if pokemon.hp()>0:
-                return True
-        return False
-
-    def add_pokemon(self, pokemon:Pokemon):
-        self._pokemons.append(pokemon)
-
-    def set_selected_pokemon(self, pokemon:Pokemon):
-        self._selected_pokemon = pokemon
-
-    def selected_pokemon(self):
-        return self._selected_pokemon
